1e59dfc01c0220d2a82286b5003145939b23e1e9
pujuneu <132837504+pujuneu@users.noreply.github.com>
2023-08-08 15:39:48 +0000

    Delete

--- resources/views/brands.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- resources/views/brands.blade.php -->
-
-@extends('master')
-
-@section('content')
-    <h2 class="font-bold text-4xl text-center my-5">Our Brands</h2>
-    <p>Add your brand information here.</p>
-@endsection
